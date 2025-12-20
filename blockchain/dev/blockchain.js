82aeaa3581b6d5d55736af2ebff50acb5f5806b0
veritas 9824 <9824@veritas.edu.ng>
2025-12-20 19:01:24 +0100

    (member 3): task_3 completed

--- blockchain/dev/blockchain.js
+++ blockchain/dev/blockchain.js
@@ -1,15 +1,90 @@
-const { Blockchain, Transaction } = require("./blockchain");
+const sha256 = require("sha256");
 
-const bakeryChain = new Blockchain();
+class Transaction {
+    constructor(productName, quantity, bakerName) {
+        this.productName = productName;
+        this.quantity = quantity;
+        this.bakerName = bakerName;
+        this.timestamp = Date.now();
+    }
 
-bakeryChain.addTransaction(
-    new Transaction("Bread", 20, "Baker John")
-);
+    isValid() {
+        if (!this.productName || !this.bakerName) return false;
+        if (this.quantity <= 0) return false;
+        return true;
+    }
+}
 
-bakeryChain.addTransaction(
-    new Transaction("Cake", 5, "Baker Mary")
-);
+class Block {
+    constructor(index, timestamp, transactions, previousHash = "") {
+        this.index = index;
+        this.timestamp = timestamp;
+        this.transactions = transactions;
+        this.previousHash = previousHash;
+        this.hash = "";
+        this.nonce = 0;
+    }
 
-bakeryChain.minePendingTransactions();
+    calculateHash() {
+        return sha256(
+            this.index +
+            this.previousHash +
+            this.timestamp +
+            JSON.stringify(this.transactions) +
+            this.nonce
+        );
+    }
 
-console.log(JSON.stringify(bakeryChain, null, 4));
+    mineBlock(difficulty) {
+        const target = "0".repeat(difficulty);
+        while (this.hash.substring(0, difficulty) !== target) {
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+        console.log("Block mined:", this.hash);
+    }
+}
+
+class Blockchain {
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
+        this.difficulty = 2;
+        this.pendingTransactions = [];
+    }
+
+    createGenesisBlock() {
+        const genesis = new Block(0, Date.now(), [], "0");
+        genesis.hash = genesis.calculateHash();
+        return genesis;
+    }
+
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    addTransaction(transaction) {
+        if (!transaction || !transaction.isValid()) {
+            throw new Error("Invalid transaction data");
+        }
+        this.pendingTransactions.push(transaction);
+    }
+
+    addBlock(newBlock) {
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.mineBlock(this.difficulty);
+        this.chain.push(newBlock);
+    }
+
+    minePendingTransactions() {
+        const block = new Block(
+            this.chain.length,
+            Date.now(),
+            this.pendingTransactions
+        );
+        this.addBlock(block);
+        this.pendingTransactions = [];
+        return block;
+    }
+}
+
+module.exports = { Blockchain, Block, Transaction };
--- blockchain/dev/test.js
+++ blockchain/dev/test.js
@@ -1,29 +1,10 @@
-const { Blockchain, Block } = require("./blockchain");
-
-const testCoin = new Blockchain();
-
-testCoin.pendingTransactions.push({ amount: 4 });
-testCoin.minePendingTransactions();
-
-testCoin.pendingTransactions.push({ amount: 4 });
-testCoin.minePendingTransactions();
-
-// Print the blockchain
-console.log(JSON.stringify(testCoin, null, 4));
 const { Blockchain, Transaction } = require("./blockchain");
 
 const bakeryChain = new Blockchain();
 
-// Member 3: adding bakery records
-bakeryChain.addTransaction(
-    new Transaction("Bread", 20, "Baker John")
-);
+bakeryChain.addTransaction(new Transaction("Bread", 20, "Baker John"));
+bakeryChain.addTransaction(new Transaction("Cake", 5, "Baker Mary"));
 
-bakeryChain.addTransaction(
-    new Transaction("Cake", 5, "Baker Mary")
-);
-
-// Member 2: mining records into a block
 bakeryChain.minePendingTransactions();
 
 console.log(JSON.stringify(bakeryChain, null, 4));
