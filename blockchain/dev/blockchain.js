10f3dce359bcadc22f058c84b2348530aa9568c1
veritas 9824 <9824@veritas.edu.ng>
2025-12-20 18:49:49 +0100

    (member 3): task_3 completed

--- blockchain/dev/blockchain.js
+++ blockchain/dev/blockchain.js
@@ -1,78 +1,15 @@
-const sha256 = require("sha256");
+const { Blockchain, Transaction } = require("./blockchain");
 
-class Block {
-    constructor(index, timestamp, data, previousHash = '') {
-        this.index = index;
-        this.timestamp = timestamp;
-        this.data = data;
-        this.previousHash = previousHash;
-        this.transactions = [];
-        this.hash = '';
-        this.nonce = 0;
-    }
+const bakeryChain = new Blockchain();
 
-    calculateHash() {
-        return sha256(
-            this.index +
-            this.previousHash +
-            this.timestamp +
-            JSON.stringify(this.data) +
-            this.nonce
-        );
-    }
+bakeryChain.addTransaction(
+    new Transaction("Bread", 20, "Baker John")
+);
 
-    // PROOF OF WORK
-    mineBlock(difficulty) {
-        const target = "0".repeat(difficulty);
+bakeryChain.addTransaction(
+    new Transaction("Cake", 5, "Baker Mary")
+);
 
-        // Keep changing nonce until hash meets difficulty requirement
-        while (this.hash.substring(0, difficulty) !== target) {
-            this.nonce++;
-            this.hash = this.calculateHash();
-        }
+bakeryChain.minePendingTransactions();
 
-        console.log(`Block mined: ${this.hash}`);
-    }
-}
-
-class Blockchain {
-    constructor() {
-        this.chain = [this.createGenesisBlock()];
-        this.difficulty = 1; // can be adjusted
-        this.pendingTransactions = [];
-    }
-
-    createGenesisBlock() {
-        const genesis = new Block(0, "17/12/25", "Genesis Block", "0");
-        genesis.hash = genesis.calculateHash();
-        return genesis;
-    }
-
-    getLatestBlock() {
-        return this.chain[this.chain.length - 1];
-    }
-
-    addBlock(newBlock) {
-        newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.mineBlock(this.difficulty);
-        this.chain.push(newBlock);
-    }
-    //  MINING LOGIC
-    minePendingTransactions() {
-        const block = new Block(
-            this.chain.length,
-            Date.now(),
-            this.pendingTransactions
-        );
-
-        this.addBlock(block);
-
-        // Clear pending transactions after mining
-        this.pendingTransactions = [];
-
-        // Return mined block for display
-        return block;
-    }
-}
-
-module.exports = { Blockchain, Block };
+console.log(JSON.stringify(bakeryChain, null, 4));
--- blockchain/dev/test.js
+++ blockchain/dev/test.js
@@ -10,3 +10,20 @@ testCoin.minePendingTransactions();
 
 // Print the blockchain
 console.log(JSON.stringify(testCoin, null, 4));
+const { Blockchain, Transaction } = require("./blockchain");
+
+const bakeryChain = new Blockchain();
+
+// Member 3: adding bakery records
+bakeryChain.addTransaction(
+    new Transaction("Bread", 20, "Baker John")
+);
+
+bakeryChain.addTransaction(
+    new Transaction("Cake", 5, "Baker Mary")
+);
+
+// Member 2: mining records into a block
+bakeryChain.minePendingTransactions();
+
+console.log(JSON.stringify(bakeryChain, null, 4));
