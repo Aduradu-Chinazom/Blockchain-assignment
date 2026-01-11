a0b606efc721eb820f448c4e320541257819f778
Chinazom <angelaaduradu@gmail.com>
2026-01-10 17:45:49 -0800

    I fixed some errors in task 5

--- blockchain/dev/blockchain.js
+++ blockchain/dev/blockchain.js
@@ -1,11 +1,11 @@
 const sha256 = require("sha256");
 
 class Transaction {
-    constructor(productName, quantity, bakerName) {
+    constructor(productName, quantity, bakerName, timestamp = Date.now()) {
         this.productName = productName;
         this.quantity = quantity;
         this.bakerName = bakerName;
-        this.timestamp = Date.now();
+        this.timestamp = timestamp;
     }
 
     isValid() {
@@ -41,15 +41,23 @@ class Block {
             this.nonce++;
             this.hash = this.calculateHash();
         }
-        console.log("Block mined:", this.hash);
     }
 }
 
 class Blockchain {
     constructor() {
-        this.chain = [this.createGenesisBlock()];
-        this.difficulty = 2;
-        this.pendingTransactions = [];
+        const savedData = this.loadFromLocalStorage();
+
+        if (savedData) {
+            this.chain = savedData.chain;
+            this.pendingTransactions = savedData.pendingTransactions;
+            this.difficulty = savedData.difficulty;
+        } else {
+            this.chain = [this.createGenesisBlock()];
+            this.pendingTransactions = [];
+            this.difficulty = 2;
+            this.saveToLocalStorage();
+        }
     }
 
     createGenesisBlock() {
@@ -67,12 +75,14 @@ class Blockchain {
             throw new Error("Invalid transaction data");
         }
         this.pendingTransactions.push(transaction);
+        this.saveToLocalStorage();
     }
 
     addBlock(newBlock) {
         newBlock.previousHash = this.getLatestBlock().hash;
         newBlock.mineBlock(this.difficulty);
         this.chain.push(newBlock);
+        this.saveToLocalStorage();
     }
 
     minePendingTransactions() {
@@ -83,38 +93,73 @@ class Blockchain {
         );
         this.addBlock(block);
         this.pendingTransactions = [];
+        this.saveToLocalStorage();
         return block;
     }
 
-    
-// Member 4
-   // VALIDATION    
-   isChainValid() {
+    // Member 4
+    isChainValid() {
         for (let i = 1; i < this.chain.length; i++) {
             const currentBlock = this.chain[i];
-            const previousBlock = this.chain[i - 1]; 
+            const previousBlock = this.chain[i - 1];
 
-            // recalculate hash and compare
             if (currentBlock.hash !== currentBlock.calculateHash()) {
-                console.log('Block ${currentBlock.index} has invalid hash.');
                 return false;
             }
-            // check previous hash link
+
             if (currentBlock.previousHash !== previousBlock.hash) {
-                console.log('Block ${currentBlock.index} previous has does not match.');
                 return false;
             }
-            // check transactions validity
+
             for (const tx of currentBlock.transactions) {
-                if (!tx.isValid()) {
-                    console.log('Block ${currentBlock.index} has invalid transaction.');
-                    return false;
-                }
+                if (!tx.isValid()) return false;
+            }
         }
-        // All blocks are valid
         return true;
     }
-}
+
+    saveToLocalStorage() {
+        const data = {
+            chain: this.chain,
+            pendingTransactions: this.pendingTransactions,
+            difficulty: this.difficulty
+        };
+        localStorage.setItem("bakeryBlockchain", JSON.stringify(data));
+    }
+
+    loadFromLocalStorage() {
+        try {
+            const data = JSON.parse(localStorage.getItem("bakeryBlockchain"));
+            if (!data) return null;
+
+            data.chain = data.chain.map(block => {
+                const rebuiltBlock = new Block(
+                    block.index,
+                    block.timestamp,
+                    block.transactions,
+                    block.previousHash
+                );
+                rebuiltBlock.hash = block.hash;
+                rebuiltBlock.nonce = block.nonce;
+                return rebuiltBlock;
+            });
+
+            data.pendingTransactions = data.pendingTransactions.map(tx =>
+                new Transaction(
+                    tx.productName,
+                    tx.quantity,
+                    tx.bakerName,
+                    tx.timestamp
+                )
+            );
+
+            return data;
+        } catch (error) {
+            console.error("Corrupted blockchain storage. Resetting...");
+            localStorage.removeItem("bakeryBlockchain");
+            return null;
+        }
+    }
 }
 
 module.exports = { Blockchain, Block, Transaction };
